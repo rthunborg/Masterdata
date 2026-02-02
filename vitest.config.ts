332d5dde4240a3133a85e12b1f82d1fe90eecdaf
RThunborg <r.alestigthunborg@gmail.com>
2026-02-02 19:35:59 +0100

    update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,15 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./tests/setup.ts'],
+    // Exclude Playwright e2e tests from Vitest
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      '**/tests/e2e/**', // Exclude Playwright tests
+    ],
   },
   resolve: {
     alias: {
